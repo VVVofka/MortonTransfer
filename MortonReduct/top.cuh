78ff6a549784cc28f7560b9d642a8a0614331fcf
VVVofka <vvvofka@yandex.ru>
2025-07-26 16:25:22 +0300

    to concert

--- MortonReduct/top.cuh
+++ MortonReduct/top.cuh
@@ -31,6 +31,8 @@ static __device__ __host__ __inline__ void get_topA4A16(const uint64_t src, uint
 	sum1 |= sum1 >> 1;  // or if 4 ( res in pos 0)
 	out4 = pack4(sum1);
 }// ===============================================================================================
+
+
 // 32 thread: in_val[1] out[16]
 static __global__ void glTop1(const uint64_t* __restrict__ in_val, __half2* __restrict__ out){
 	__shared__ uint64_t src, a4, a16;
@@ -38,16 +40,16 @@ static __global__ void glTop1(const uint64_t* __restrict__ in_val, __half2* __re
 
 	if(threadIdx.x == 0){
 		src = in_val[0];
-		get_topA4A16(src, a4, a16);
+		get_topA4A16(src, a4, a16); // a4: 4 bit used only; a16: 16 bit used only;
 	}
-	__syncthreads();	
+	__syncthreads();
 
 	if(threadIdx.x < 2){
 		const __half* pkf_1 = &kF4[a4 * 4];
 		const __half2* pkf2_1 = reinterpret_cast<const __half2*>(pkf_1);
 		f0[threadIdx.x] = pkf2_1[threadIdx.x] * kLay[0];
 	}
-	__syncthreads();	
+	__syncthreads();
 
 	if(threadIdx.x < 8){
 		const uint64_t a_2 = (a16 >> (4 * (threadIdx.x / 2))) & 0xF;
@@ -68,42 +70,32 @@ static __global__ void glTop1(const uint64_t* __restrict__ in_val, __half2* __re
 	out[threadIdx.x] = res_3;
 }// ----------------------------------------------------------------------------------------------
 
-template <unsigned COUNT_LAYS = 3>
+template <unsigned COUNT_LAYS>
 static __global__ void glTop1N(const uint64_t* __restrict__ in_val, __half2* __restrict__ out){
 	__shared__ uint64_t a4, a16;
-	__shared__ __half2 f0[(COUNT_LAYS & 1) ? 1 << (COUNT_LAYS - 2) : 1 << COUNT_LAYS];
-	__shared__ __half2 f1[(COUNT_LAYS & 1) ? 1 << COUNT_LAYS : 1 << (COUNT_LAYS - 2)];
+//	__shared__ __half2 f0[...], f1[...];	// TODO:
 
 #pragma unroll
 	for(unsigned nlay = 0; nlay < COUNT_LAYS; nlay++){
-
+		// TODO: add
 	}
-
+	// ...
 	if(threadIdx.x == 0){
-		get_topA4A16(in_val[0], a4, a16);
+		//get_topA4A16(src, a4, a16); // a4: 4 bit used only; a16: 16 bit used only;
 	}
-	__syncthreads();	// __syncwarp();
+	__syncthreads();
 
 	if(threadIdx.x < 2){
 		const __half* pkf_1 = &kF4[a4 * 4];
-		const __half2* pkf2_2 = reinterpret_cast<const __half2*>(&pkf_1);
-		f0[threadIdx.x] = pkf2_2[threadIdx.x] * kLay[0];	//if(threadIdx.x < 2)!
+		const __half2* pkf2_1 = reinterpret_cast<const __half2*>(pkf_1);
+		//f0[threadIdx.x] = pkf2_1[threadIdx.x] * kLay[0];
 	}
-	__syncthreads();	// __syncwarp();
+	__syncthreads();
 
-	if(threadIdx.x < 8){
-		const uint64_t a_2 = (a16 >> (4 * (threadIdx.x / 8))) & 0xF;	// (threadIdx.x / 8)=[0..3]
-		const __half* pkf_2 = &kF4[a_2 * 4];
-		const __half2* pkf2_2 = reinterpret_cast<const __half2*>(&pkf_2);
-		f1[threadIdx.x] = f0[threadIdx.x / 16] + pkf2_2[threadIdx.x & 1] * kLay[1];
+	if(threadIdx.x < 8){}	// TODO: move to for?
+
+#pragma unroll
+	for(unsigned nlay = 0; nlay < COUNT_LAYS; nlay++){
+		// TODO: add
 	}
-	__syncthreads();	// __syncwarp();
-
-	const uint64_t a64 = in_val[0];
-	const uint64_t a_3 = (a64 >> (4 * (threadIdx.x / 2))) & 0xF;	// (threadIdx.x / 2)=[0..15]
-	const __half* pkf_3 = &kF4[a_3 * 4];
-	const __half2* pkf2_3 = reinterpret_cast<const __half2*>(&pkf_3);
-	const __half2 res_3 = f1[threadIdx.x / 4] + pkf2_3[threadIdx.x & 1] * kLay[2];
-
-	out[threadIdx.x] = res_3;
 }// ----------------------------------------------------------------------------------------------
