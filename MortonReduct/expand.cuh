07907fef62f53b2b9c77d6c506660808f1bc5a03
VVVofka <vvvofka@yandex.ru>
2025-08-15 15:53:30 +0300

    x64 +

--- MortonReduct/expand.cuh
+++ MortonReduct/expand.cuh
@@ -28,6 +28,7 @@ void x64(const __half2* __restrict__ pfup,
 			__half2* __restrict__ pdst){
 	__shared__ __half2 fup2;
 	__shared__ __half2 midf1[4];
+	__shared__ __half2 midf0[16];
 	__shared__ uint32_t mida1;	// 8 bit only used
 	__shared__ uint32_t mida0;	// 32 bit used
 
@@ -46,42 +47,30 @@ void x64(const __half2* __restrict__ pfup,
 		midf1[threadIdx.x] = kf * klays[2] + __half2half2(fup);
 	}
 	syncwarp();
-
-	const uint32_t shift = ((blockIdx.x & 1) * 4 + threadIdx.x / 4) * 4;
-	const uint32_t maska = (mida1 >> shift) & 0xF;
-	const __half2 kf = reinterpret_cast<const __half2*>(kF4 + maska * 4)[threadIdx.x & 1];
-	const __half fup = threadIdx.x & 2 ? fup2.y : fup2.x;
-	midf1[threadIdx.x] = kf * klays[2] + __half2half2(fup);
-
-
-	const unsigned id_in = blockIdx.x * 8 + threadIdx.x;
-	uint32_t tmp = uint32_t(pack16(convolution64to16(psrc[id_in])));
-	uint32_t& m = mid[threadIdx.x / 2];
-	m = threadIdx.x & 1 ? 0 : tmp;
-	syncwarp();
-	if(threadIdx.x & 1)
-		m |= tmp << 16;
-	syncwarp();
-
-	if((threadIdx.x & 1) == 0)
-		m = pack8(convolution32to8(m));
-	syncwarp();
-
-
-	const unsigned id_out = blockIdx.x * 8 + threadIdx.x;
-	uint32_t tmp = uint32_t(pack16(convolution64to16(psrc[id_in])));
-	uint32_t& m = mid[threadIdx.x / 2];
-	m = threadIdx.x & 1 ? 0 : tmp;
-	syncwarp();
-	if(threadIdx.x & 1)
-		m |= tmp << 16;
-	syncwarp();
-
-	if((threadIdx.x & 1) == 0)
-		m = pack8(convolution32to8(m));
-	syncwarp();
-
-	if(threadIdx.x == 0)
-		*pdst = mid[0] | (mid[1] << 8) | (mid[2] << 16) | (mid[3] << 24);
+	{
+		const uint32_t shift = (threadIdx.x / 2) * 4;
+		const uint32_t maska = (mida0 >> shift) & 0xF;
+		const __half2 kf = reinterpret_cast<const __half2*>(kF4 + maska * 4)[threadIdx.x & 1];
+		const __half fup = threadIdx.x & 1 ? midf1[threadIdx.x / 4].y : midf1[threadIdx.x / 4].x;
+		midf0[threadIdx.x] = kf * klays[1] + __half2half2(fup);
+	}
+	{
+		const uint32_t ina = pdna[blockIdx.x * blockDim.x * 4 + threadIdx.x / 4];
+		__half2* pout = pdst + (blockIdx.x * blockDim.x + threadIdx.x) * 4;
+		{
+			const uint32_t maska = (ina >> (threadIdx.x * 8)) & 0xF;
+			const __half2* kf = reinterpret_cast<const __half2*>(kF4 + maska * 4);
+			const __half2 fup = __half2half2(midf0[threadIdx.x].x);
+			pout[0] = kf[0] * klays[0] + fup;
+			pout[1] = kf[1] * klays[0] + fup;
+		}
+		{
+			const uint32_t maska = (ina >> (threadIdx.x * 8 + 4)) & 0xF;
+			const __half2* kf = reinterpret_cast<const __half2*>(kF4 + maska * 4);
+			const __half2 fup = __half2half2(midf0[threadIdx.x].y);
+			pout[2] = kf[0] * klays[0] + fup;
+			pout[3] = kf[1] * klays[0] + fup;
+		}
+	}
 } // ----------------------------------------------------------------------------------------------
 } // @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
